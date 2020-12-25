8f3df3a7ffee407a291b2390e2d303269a141e6b
Mykola Osadchyi <osadchiy_n@ukr.net>
2020-12-25 02:32:37 +0200

    fix-1

--- src/services/imagesApi.js
+++ src/services/imagesApi.js
@@ -1,7 +1,7 @@
 const API_KEY = '18724736-77330c9d8a28eb7073d2e9b7d';
 
 const fetchImages = (query, page) => {
-  const url = `https://pixabay.com/api/?key=${API_KEY}&q=${query}&page=${page}&image_type=photo&orientation=horizontal&per_page=4`;
+  const url = `https://pixabay.com/api/?key=${API_KEY}&q=${query}&page=${page}&image_type=photo&orientation=horizontal&per_page=12`;
   return fetch(url).then(response => {
     if (response.ok) {
       return response.json();
